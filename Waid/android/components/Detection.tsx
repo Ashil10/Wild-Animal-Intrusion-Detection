1bf653a2d18390cd517e9855da97b6985f43340b
Ashil <ashilkp2003@gmail.com>
2024-12-17 00:50:34 +0530

    Delete Waid/android/components directory

--- Waid/android/components/Detection.tsx
+++
@@ -1,8 +0,0 @@
-import { firestore } from 'firebase-admin';
-
-// Define the type for the animal detection data
-export type Detection = {
-  id: string;
-  animalName: string;
-  timestamp: firestore.Timestamp; // Use Firestore Timestamp type for timestamp
-};
